130695b15674f0360acf294d004e94e02829e8fc
alex steve <asteve2018@gmail.com>
2018-08-21 10:32:50 +0200

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_160215) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
