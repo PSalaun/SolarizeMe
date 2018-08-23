1407756da18e153f2e9b5fb6f7fac4331d58e849
alex steve <asteve2018@gmail.com>
2018-08-23 12:42:01 +0200

    adds columns to products, better financial indicators

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_134734) do
+ActiveRecord::Schema.define(version: 2018_08_23_095102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2018_08_22_134734) do
     t.date "crowdfunding_start_date"
     t.date "crowdfunding_end_date"
     t.date "comissioning_date"
+    t.integer "roi_decimals"
+    t.integer "kwh_price_cents", default: 0, null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
