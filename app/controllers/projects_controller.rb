64648b052b29f40165bfb9700a2024396e93bc1a
Pierre Salaun <pierresalaun10@hotmail.com>
2018-08-27 17:47:35 +0200

    filters work on index page. need to improve design

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,14 @@ class ProjectsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if params[:query].present?
-      @projects = policy_scope(Project).where(country: params[:query])
+    # raise
+    if params[:location].present?
+      @projects = policy_scope(Project).where(country: params[:location])
+    elsif params[:status].present?
+      @projects = policy_scope(Project).all.select { |project| params[:status].include?(project.status)}
+    elsif params[:location].present? && params[:status].present?
+      @location_filtered_projects = policy_scope(Project).where(country: params[:location])
+      @projects = @location_filtered_projects.select { |project| params[:status].include?(project.status)}
     else
       @projects = policy_scope(Project).all
     end
