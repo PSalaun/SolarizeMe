02d970ef79b550a9275dda75e4c5049f273e04d4
alex steve <asteve2018@gmail.com>
2018-08-27 20:21:15 +0200

    changes chart per day, loads api data by hour, corrects formulas in various places

--- app/services/forecast_api.rb
+++ app/services/forecast_api.rb
@@ -4,27 +4,49 @@ require "rest-client"
 class ForecastApi
   def initialize(project_id)
     # Fetch the project from the database
-    @project = Project.find(project_id)
+    puts project_id
+    @project = Project.find(project_id.to_i)
+    puts @project
+
   end
 
 
   def generate_output
     # fetch  latitude, longitude, etc. and insert them in the url (so url is dynamic)
-    lat = @project.lat
-    lon = @project.lon
+    lat = @project.lat.class
+    lon = @project.lon.class
     kwc = @project.kwc
 
     # API CALL
-    response = RestClient.get "https://api.forecast.solar/estimate/watthours/day/#{lat}/#{lon}/37/0/#{kwc}"
+    # response = RestClient.get "https://api.forecast.solar/estimate/52.0/12.0/37/0/324.02"
+    response = RestClient.get "https://api.forecast.solar/estimate/#{lat.to_s}/#{lon.to_s}/37/0/#{kwc.to_s}"
+
     repos = JSON.parse(response)
-    repos_date = repos['result'].first
-    if repos_date
-      value = repos_date[1]
-      # with json response, create a new output with the information you get
-      Output.create(date: Date.today, project: @project, quantity: value)
-    else
-      Output.create(date: Date.today, project: @project, quantity: 101)
+    repos_watt = repos['result']['watts']
+    date = Date.today.to_s
+
+    repos_day_production = repos['result']['watt_hours_day'][date]
+    repos_watt.each do |key, value|
+      output = Output.new ()
+      city = Project.where(name: "Paris Offices").first
+      output.project = city
+      output.detailedtime = DateTime.parse(key)
+      output.date = DateTime.parse(key).to_date
+      output.quantity = repos_day_production
+      output.production = value
+
+      if output.date == Date.today
+        output.save!
+      end
     end
+
+    # if repos_date
+    #   value = repos_date[1]
+    #   # with json response, create a new output with the information you get
+    #   Output.create(date: Date.today, project: @project, quantity: value)
+    # else
+    #   Output.create(date: Date.today, project: @project, quantity: 105)
+    # end
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_093143) do
+ActiveRecord::Schema.define(version: 2018_08_27_152617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2018_08_27_093143) do
     t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "detailedtime"
+    t.integer "production"
     t.index ["project_id"], name: "index_outputs_on_project_id"
   end
 
